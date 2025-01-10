41e0deeeb69a203315264030da62da54504627d7
temesgenmotuma <janijesus674@gmail.com>
2025-01-10 19:00:29 +0300

    git ignore

--- obj/Debug/net8.0/pokemon.AssemblyInfo.cs
+++ obj/Debug/net8.0/pokemon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pokemon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6f3b560eedbc4766a1b3189293b926be26d605")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa6973e0adf14045692b83b13602faf76b7d47f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("pokemon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pokemon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
